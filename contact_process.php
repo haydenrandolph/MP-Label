672c3ef4ca2aa945c0cc591155cb230347a27e9a
MadDawg46 <keenanwilliamson97@gmail.com>
2019-04-12 11:39:47 -0500

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "xaxprivatelabel@gmail.com";
+    $to = "maxprivatelabel@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
